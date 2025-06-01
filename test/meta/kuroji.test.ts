8043757658975e49ff4c80113b7bea81dc7fb185
Mohtasim Alam <94981761+shimizudev@users.noreply.github.com>
2025-06-01 18:10:57 +0600

    feat: use bun tests

--- test/meta/kuroji.test.ts
+++ test/meta/kuroji.test.ts
@@ -1,39 +1,38 @@
-import { META } from '../../src'
-import Config from '../../src/config'
-import { FilterDto } from '../../src/providers/meta/kuroji/types/FilterDto'
-import { EProvider } from '../../src/providers/meta/kuroji/types/types'
-
-jest.setTimeout(30000)
+import { expect, test } from 'bun:test';
+import { META } from '../../src';
+import Config from '../../src/config';
+import { FilterDto } from '../../src/providers/meta/kuroji/types/FilterDto';
+import { EProvider } from '../../src/providers/meta/kuroji/types/types';
 
 const kuroji = new META.Kuroji();
-kuroji.setBaseUrl(Config.KUROJI_URL)
+kuroji.setBaseUrl(Config.KUROJI_URL);
 
 test('returns info of anime', async () => {
-  const data = await kuroji.getInfo(21);
-  expect(data).not.toEqual({});
-})
+	const data = await kuroji.getInfo(169927);
+	expect(data).not.toEqual({});
+}, 30000);
 
 test('returns a search of anime list', async () => {
-  const data = await kuroji.search('Naruto');
-  expect(data.data).not.toEqual([]);
-})
+	const data = await kuroji.search('Alya Sometimes Hides Her Feelings In Russian');
+	expect(data.data).not.toEqual([]);
+}, 30000);
 
 test('returns a filter of anime list', async () => {
-  const data = await kuroji.getFilter(new FilterDto({ id: 21 }));
-  expect(data.data).not.toEqual([]);
-})
+	const data = await kuroji.getFilter(new FilterDto({ id: 169927 }));
+	expect(data.data).not.toEqual([]);
+}, 30000);
 
 test('returns episodes', async () => {
-  const data = await kuroji.getEpisodes(21);
-  expect(data).not.toEqual([]);
-})
+	const data = await kuroji.getEpisodes(21);
+	expect(data).not.toEqual([]);
+}, 30000);
 
 test('returns providers', async () => {
-  const data = await kuroji.getProviders(21)
-  expect(data).not.toEqual([]);
-})
+	const data = await kuroji.getProviders(169927);
+	expect(data).not.toEqual([]);
+}, 30000);
 
 test('returns sources for episode and provider', async () => {
-  const data = await kuroji.getSources(21, 1, EProvider.zoro)
-  expect(data).not.toEqual([]);
-})
+	const data = await kuroji.getSources(169927, 1, EProvider.zoro);
+	expect(data).not.toEqual([]);
+}, 30000);
